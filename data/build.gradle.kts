664f0a6557d37a653155a15e6eb27e9a4e2e6d75
Ksenia M <kmochalova@sfedu.ru>
2025-09-03 19:56:04 +0300

    login layout

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -33,11 +33,7 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(project(":domain"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
